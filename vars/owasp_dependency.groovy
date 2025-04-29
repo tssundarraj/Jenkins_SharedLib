599dac231ae29fa50bdfe611fcdfce5bd77899d4
T S Sundar Raj <tssundarraj08@gmail.com>
2025-04-29 11:08:19 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,8 @@
 def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
+  dependencyCheck(
+    additionalArguments: '--scan ./ --nvdApiKey f1f92557-d196-4d3f-bec2-72e1fe4d6b0c',
+    odcInstallation: 'OWASP'
+  )
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+  archiveArtifacts artifacts: '**/dependency-check-report.*', allowEmptyArchive: true
 }
